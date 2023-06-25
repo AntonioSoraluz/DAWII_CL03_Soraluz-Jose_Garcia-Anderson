447002c7151ff9a78967e919b9efb3589840657f
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-06-25 14:05:17 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DAWII_CL03_Soraluz-Jose_Garcia-Anderson';
+}
