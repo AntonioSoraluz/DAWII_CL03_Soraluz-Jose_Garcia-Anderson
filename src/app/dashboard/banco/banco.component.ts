8216e155af0a259285f88ca780cc10d50c4f4cea
ANDERSON <i202020454@cibertec.edu.pe>
2023-06-25 18:36:22 -0500

    Se agrego la tabla para que liste los bancos

--- src/app/dashboard/banco/banco.component.ts
+++ src/app/dashboard/banco/banco.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Banco} from "./banco";
+import {BancoService} from "./banco.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-banco',
   templateUrl: './banco.component.html',
   styleUrls: ['./banco.component.scss']
 })
-export class BancoComponent {
+export class BancoComponent implements OnInit {
 
+  bancos: Banco[] = [];
+  displayedColumns: string[] = ['id_banco', 'nombre_banco'];
+
+  constructor(
+    private bancoService: BancoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.bancoService.listarBancos()
+      .subscribe((data) => {
+        this.bancos = data
+      });
+  }
+
+  onNavigateToDetail(id: number): void {
+    this.router.navigate([id], {relativeTo: this.route});
+  }
 }
