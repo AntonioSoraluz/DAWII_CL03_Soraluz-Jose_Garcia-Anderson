c43fe0ca95585cde2f8e8ba8d1f54b4025dba0f9
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-06-25 15:47:44 -0500

    Se añadió una pequeña validación al ingresar las notas, min 0, max 20 y no debe estar vacío el campo.

--- src/app/dashboard/promedio/promedio.component.ts
+++ src/app/dashboard/promedio/promedio.component.ts
@@ -13,8 +13,23 @@ export class PromedioComponent {
     prom: 0,
   }
   resultado= "";
-  calcularPROM(){
-    this.frmProm.prom = (Number(this.frmProm.nota1)+Number(this.frmProm.nota2)+Number(this.frmProm.nota3)) / 3
-    this.resultado = "Su Promedio es de "+ this.frmProm.prom;
+  calcularPROM() {
+    if (
+      this.frmProm.nota1 === null ||
+      this.frmProm.nota2 === null ||
+      this.frmProm.nota3 === null ||
+      this.frmProm.nota1 < 0 ||
+      this.frmProm.nota1 > 20 ||
+      this.frmProm.nota2 < 0 ||
+      this.frmProm.nota2 > 20 ||
+      this.frmProm.nota3 < 0 ||
+      this.frmProm.nota3 > 20
+    ) {
+      this.resultado = "Debe ingresar las notas. La nota mínima es 0 y la nota máxima es 20.";
+    } else {
+      this.frmProm.prom = (Number(this.frmProm.nota1) + Number(this.frmProm.nota2) + Number(this.frmProm.nota3)) / 3;
+      this.resultado = "Su Promedio es de " + this.frmProm.prom;
+    }
   }
+  
 }
